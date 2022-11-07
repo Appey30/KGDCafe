f18850f72723cd71e2cd5b46a48c9af3453ddf8f
Appey30 <appey_30@yahoo.com>
2022-11-07 10:53:30 +0800

    settingsadd

--- ProjectAutomation/settings.py
+++ ProjectAutomation/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,6 +137,7 @@ MEDIAFILES_DIRS=[
     ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+django_heroku.settings(locals())
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
