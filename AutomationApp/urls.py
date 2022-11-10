a48a936685709b4256a0a26ec22c2d9c22e9615b
Appey30 <appey_30@yahoo.com>
2022-11-10 22:59:29 +0800

    tryagain46

--- AutomationApp/urls.py
+++ AutomationApp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 
 #path('add',views.addproduct, name='addprod.html'), 
 #path('/<str:prodd_id>', views.dproduct , name='deleteprod'),
-path('<del_id>', views.delprod , name='delprod'),
-path('edit/<edit_id>', views.editprod , name='editprod'),
+path('<int:del_id>', views.delprod , name='delprod'),
+path('edit/<int:edit_id>', views.editprod , name='editprod'),
 ]
