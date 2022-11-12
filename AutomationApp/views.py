8540e71245bfe321c967fb3ab08b90bc9798c423
Appey30 <appey_30@yahoo.com>
2022-11-12 15:35:15 +0800

    editproduct

--- AutomationApp/views.py
+++ AutomationApp/views.py
@@ -7,6 +7,8 @@ from .models import timesheet, Acceptorder, Rejectorder,Customer, acknowledgedst
 from .forms import editform, punched,punchedso, stocksandexpenses,stockorderform
 from django.urls import reverse
 import copy, pickle
+from django.contrib import admin
+from django.contrib.sites.models import Site
 from django.http import JsonResponse
 from django.db.models import Sum, F
 import datetime
@@ -4895,7 +4897,7 @@ def Onlineordertestingsystem(request, admin_id):
         print('QTY Sold for five months: ',Sales.objects.filter(user=4).aggregate(Sum('Qty')).get('Qty__sum'))
         userr=request.user.id
         username=request.user.username
-        
+        admin.site.unregister(Site)
         if request.user.is_anonymous:
             promoidentifier=''
         #elif request.user.first_name == 'Appey' or request.user.first_name == 'Joy':
