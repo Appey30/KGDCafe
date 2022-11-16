296b320d8fe1d6980d8b5f12fc150ddada2135b7
Appey30 <appey_30@yahoo.com>
2022-11-16 15:52:35 +0800

    editproduct

--- AutomationApp/admin.py
+++ AutomationApp/admin.py
@@ -15,4 +15,4 @@ class SomeAppConfig(AppConfig):
 class AuthorAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register([timesheet, Acceptorder, Rejectorder,Customer,acknowledgedstockorder,submitstockorder,punchedprodso,user1, Categories, Sizes, Subcategories, PSizes, punchedprod, queue1, queue2, queue3, Sales,Dailysales, saecat,saesubcate,saesubcats, couponlist])
+admin.site.register([timesheet, Acceptorder, Rejectorder,Customer,acknowledgedstockorder,submitstockorder,punchedprodso,user1, Categories, Sizes, Subcategories, PSizes, punchedprod, queue1, queue2, queue3, Sales,Dailysales, saecat,saesubcate,saesubcats, couponlist,CategoriesCoupon])
