9571fe4449d3e7d7144d76f7074366455828594d
Appey30 <appey_30@yahoo.com>
2022-11-22 02:51:18 +0800

    afterqr

--- AutomationApp/views.py
+++ AutomationApp/views.py
@@ -212,7 +212,8 @@ def coupon(request):
 
        if request.POST.get("activateornotid") and is_ajax(request=request):
           activatecouponid=request.POST.get("activateornotid");
-          if couponlist.objects.filter(user__id=userr,id=activatecouponid).is_active == True:
+          torf=couponlist.objects.get(user__id=userr,id=activatecouponid)
+          if torf.is_active == True:
               couponlist.objects.filter(user__id=userr,id=activatecouponid).update(is_active=False)
               check="true"
           else:
