cd2cbf7164e8c68b276f5924f020cee537fae91a
Appey30 <appey_30@yahoo.com>
2022-12-02 03:35:20 +0800

    owndomain

--- AutomationApp/views.py
+++ AutomationApp/views.py
@@ -3236,9 +3236,10 @@ def orderprogress(request, admin_id):
             completename=''
             #firstname=''
             #lastname=''
-        print('completename:',completename)
+        
         if is_ajax(request=request) and request.GET.get('progressETA'):
             completenamefinal=request.GET.get('completename')
+            print('completenamefinal:',completenamefinal)
             if Acceptorder.objects.filter(Admin=admin_id, Customername=completenamefinal):
                 ETAi=Acceptorder.objects.filter(Admin=admin_id, Customername=completenamefinal).values_list('ETA', flat=True).first()
             else:
