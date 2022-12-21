ab129f0b74e06495196f8ae1327c792c5b686b34
Appey30 <appey_30@yahoo.com>
2022-12-21 17:17:17 +0800

    refine

--- AutomationApp/urls.py
+++ AutomationApp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 app_name = "AutomationApp"
@@ -8,5 +8,5 @@ urlpatterns = [
 #path('/<str:prodd_id>', views.dproduct , name='deleteprod'),
 path('<int:del_id>', views.delprod , name='delprod'),
 path('edit/<int:edit_id>', views.editprod , name='editprod'),
-re_path(r'^85f449d37a9f01958ed52ed4b0491315f6fbbf7b7bebba424f96b9f23bc9/$', views.FacebookWebhookView.as_view(), name='webhook'),
+re_path(r'^api/fb/webhook/85f449d37a9f01958ed52ed4b0491315f6fbbf7b7bebba424f96b9f23bc9/$', views.FacebookWebhookView.as_view(), name='webhook'),
 ]
