7b92719d1ee631c639941f4c783ca1574880756f
Appey30 <appey_30@yahoo.com>
2022-12-21 17:47:44 +0800

    refine

--- AutomationApp/urls.py
+++ AutomationApp/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
 path('<int:del_id>', views.delprod , name='delprod'),
 path('edit/<int:edit_id>', views.editprod , name='editprod'),
 #re_path(r'^api/fb/webhook/85f449d37a9f01958ed52ed4b0491315f6fbbf7b7bebba424f96b9f23bc9/$', views.FacebookWebhookView.as_view(), name='webhook'),
-path('webhook/85f449d37a9f01958ed52ed4b0491315f6fbbf7b7bebba424f96b9f23bc9', views.FacebookWebhookView.as_view(), name='webhook'),
+path('webhook/<webhook_endpoint>', views.FacebookWebhookView.as_view(), name='webhook'),
 ]
