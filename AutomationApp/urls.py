c2ad689d3afed46244c06482cc787d2d3c7999b6
Appey30 <appey_30@yahoo.com>
2022-12-22 18:39:25 +0800

    refine

--- AutomationApp/urls.py
+++ AutomationApp/urls.py
@@ -10,5 +10,5 @@ path('<int:del_id>', views.delprod , name='delprod'),
 path('edit/<int:edit_id>', views.editprod , name='editprod'),
 #re_path(r'^api/fb/webhook/85f449d37a9f01958ed52ed4b0491315f6fbbf7b7bebba424f96b9f23bc9/$', views.FacebookWebhookView.as_view(), name='webhook'),
 #re_path(r'^webhook/85f449d37a9f01958ed52ed4b0491315f6fbbf7b7bebba424f96b9f23bc9/$', views.FacebookWebhookView.as_view(), name='webhook'),
-path(r'^85f449d37a9f01958ed52ed4b0491315f6fbbf7b7bebba424f96b9f23bc9/?$', views.FacebookWebhookView.as_view()) 
+re_path(r'^85f449d37a9f01958ed52ed4b0491315f6fbbf7b7bebba424f96b9f23bc9/?$', views.FacebookWebhookView.as_view()) 
 ]
