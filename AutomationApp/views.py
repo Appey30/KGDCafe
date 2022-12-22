a671e9b58d0d519eba773f4ba9d534180b4cba1c
Appey30 <appey_30@yahoo.com>
2022-12-22 18:27:41 +0800

    refine

--- AutomationApp/views.py
+++ AutomationApp/views.py
@@ -41,71 +41,77 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from .logic import LOGIC_RESPONSES
 
-VERIFY_TOKEN =  os.environ.get('VERIFY_TOKEN') # generated above
+PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
+VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
 
-"""
-FB_ENDPOINT & PAGE_ACCESS_TOKEN
-Come from the next step.
-"""
+jokes = { 'yahoo': ["""reply yahoo""", 
+                     """reply yahoo two"""], 
+         'discount':      ["""reply discount""", 
+                      """ reply discount two" """], 
+         'discountdiscount': ["""reply discountdiscount""", 
+                  """reply discountdiscount two"""] }
 
-
-
-def parse_and_send_fb_message(fbid, recevied_message):
+# Helper function
+def post_facebook_message(fbid, recevied_message):
     # Remove all punctuations, lower case the text and split it based on space
     tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
-    msg = None
+    joke_text = ''
     for token in tokens:
-        if token in LOGIC_RESPONSES:
-            msg = random.choice(LOGIC_RESPONSES[token])
-            #msg = LOGIC_RESPONSES['Discount']
+        if token in jokes:
+            joke_text = random.choice(jokes[token])
             break
+    if not joke_text:
+        joke_text = "I didn't understand!!" 
 
-    if msg is not None: 
-        
-        endpoint = os.environ.get('endpoint')
-        response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":msg}})
-        status = requests.post(
-            endpoint, 
-            headers={"Content-Type": "application/json"},
-            data=response_msg)
-        print(status.json())
-        return stats.json()
-    return None
+    user_details_url = "https://graph.facebook.com/v15.0/%s"%fbid 
+    user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
+    user_details = requests.get(user_details_url, user_details_params).json() 
+    joke_text = 'Yo '+user_details['first_name']+'..! ' + joke_text
+                   
+    post_message_url = 'https://graph.facebook.com/v15.0/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    pprint(status.json())
 
-
+# Create your views here.
 class FacebookWebhookView(View):
-    @method_decorator(csrf_exempt) # required
-    def dispatch(self, request, *args, **kwargs):
-        print('Dispatched self:',self)
-        print('Dispatched request:',request)
-        return super(FacebookWebhookView, self).dispatch(request, *args, **kwargs) #python3.6+ syntax
-
     def get(self, request, *args, **kwargs):
-        print('get self:',self)
-        print('get request:',request)
+        if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error, invalid token')
+        
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return View.dispatch(self, request, *args, **kwargs)
 
-        hub_mode   = request.GET.get('hub.mode')
-        print('hub_mode: ', hub_mode)
-        hub_token = request.GET.get('hub.verify_token')
-        print('hub_token: ', hub_token)
-        hub_challenge = request.GET.get('hub.challenge')
-        print('hub_challenge: ', hub_challenge)
-        if hub_token != VERIFY_TOKEN:
-            print('VERIFY_TOKEN: ', VERIFY_TOKEN)
-            return HttpResponse('Error, invalid token', status=403)
-        return HttpResponse(hub_challenge)
-
-
+    # Post function to handle Facebook messages
     def post(self, request, *args, **kwargs):
-        incoming_message = json.loads(request.body.decode('utf-8'))
+        # Converts the text payload into a python dictionary
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        # Facebook recommends going through every entry since they might send
+        # multiple messages in a single call during high load
         for entry in incoming_message['entry']:
             for message in entry['messaging']:
+                # Check to make sure the received call is a message call
+                # This might be delivery, optin, postback for other events 
                 if 'message' in message:
-                    fb_user_id = message['sender']['id'] # sweet!
-                    fb_user_txt = message['message'].get('text')
-                    if fb_user_txt:
-                        parse_and_send_fb_message(fb_user_id, fb_user_txt)
-        return HttpResponse("Success", status=200)
+                    # Print the message to the terminal
+                    pprint(message)    
+                    # Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
+                    # are sent as attachments and must be handled accordingly. 
+                    post_facebook_message(message['sender']['id'], message['message']['text'])    
+        return HttpResponse()    
+
+
+
+
+
+
+
+
+########################################################
+
 
 
 
