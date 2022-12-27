7893048655d85e74d8bcc6a82d5baaad8b2ec6c4
Appey30 <appey_30@yahoo.com>
2022-12-27 15:57:54 +0800

    refine

--- AutomationApp/views.py
+++ AutomationApp/views.py
@@ -103,22 +103,26 @@ class FacebookWebhookView(View):
         return View.dispatch(self, request, *args, **kwargs)
 
     # Post function to handle Facebook messages
-#    def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         # Converts the text payload into a python dictionary
-#        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        print('1')
         # Facebook recommends going through every entry since they might send
         # multiple messages in a single call during high load
-#        for entry in incoming_message['entry']:
-#            for message in entry['messaging']:
+        for entry in incoming_message['entry']:
+            for message in entry['messaging']:
                 # Check to make sure the received call is a message call
                 # This might be delivery, optin, postback for other events 
-#                if 'message' in message:
+                print('2')
+                if 'message' in message:
+                    print('3')
                     # Print the message to the terminal
-#                    pprint(message)    
+                    pprint(message)    
                     # Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
                     # are sent as attachments and must be handled accordingly. 
-#                    post_facebook_message(message['sender']['id'], message['message']['text'])    
-#        return HttpResponse()    
+                    post_facebook_message(message['sender']['id'], message['message']['text'])    
+        print('5')
+        return HttpResponse()    
 
 
 
