29a27b3a29e67d1cde6245f9088833b449088c43
Appey30 <appey_30@yahoo.com>
2022-12-27 15:52:12 +0800

    refine

--- AutomationApp/views.py
+++ AutomationApp/views.py
@@ -93,7 +93,7 @@ class FacebookWebhookView(View):
             mode=False
         if token == VERIFY_TOKEN and mode=='subscribe':
             print('Congrats! Webhook_verified')
-            return HttpResponse(self.request.GET['hub.challenge'])
+            return HttpResponse('Challenge: '+self.request.GET['hub.challenge']+'     mode: '+mode)
         else:
             print('hub.verify_token: ',token)
             return HttpResponse('Error, invalid token')
@@ -103,22 +103,22 @@ class FacebookWebhookView(View):
         return View.dispatch(self, request, *args, **kwargs)
 
     # Post function to handle Facebook messages
-    def post(self, request, *args, **kwargs):
+#    def post(self, request, *args, **kwargs):
         # Converts the text payload into a python dictionary
-        incoming_message = json.loads(self.request.body.decode('utf-8'))
+#        incoming_message = json.loads(self.request.body.decode('utf-8'))
         # Facebook recommends going through every entry since they might send
         # multiple messages in a single call during high load
-        for entry in incoming_message['entry']:
-            for message in entry['messaging']:
+#        for entry in incoming_message['entry']:
+#            for message in entry['messaging']:
                 # Check to make sure the received call is a message call
                 # This might be delivery, optin, postback for other events 
-                if 'message' in message:
+#                if 'message' in message:
                     # Print the message to the terminal
-                    pprint(message)    
+#                    pprint(message)    
                     # Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
                     # are sent as attachments and must be handled accordingly. 
-                    post_facebook_message(message['sender']['id'], message['message']['text'])    
-        return HttpResponse()    
+#                    post_facebook_message(message['sender']['id'], message['message']['text'])    
+#        return HttpResponse()    
 
 
 
