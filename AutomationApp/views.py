ad88dc53ee7ee3460ff394630c64d6550d259333
Appey30 <appey_30@yahoo.com>
2023-01-09 04:52:01 +0800

    refine

--- AutomationApp/views.py
+++ AutomationApp/views.py
@@ -5512,8 +5512,10 @@ def Onlineordersystem(request, admin_id):
             user_details_params = {'fields':'first_name, last_name,id', 'access_token':os.environ.get('USER_ACCESS_TOKEN')} 
             user_details = requests.get(user_details_url, user_details_params).json() 
             fbidi=user_details['id']
-            userid = social.uid
-            print('userid: ',userid)
+            useridone = social_user.uid
+            print('useridone: ',useridone)
+            useridtwo = social.uid
+            print('useridtwo: ',useridtwo)
             print('social_auth: ',fbidi)
             #fbidi=SocialAccount.objects.filter(user=request.user, provider='facebook')[0].uid
             return HttpResponseRedirect('/messengershop/item/'+str(product_id)+'/?id='+fbidi)
