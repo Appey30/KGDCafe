6aff0e835063033547361fcff2567f605f3a47a7
Appey30 <appey_30@yahoo.com>
2023-01-09 04:32:39 +0800

    refine

--- AutomationApp/views.py
+++ AutomationApp/views.py
@@ -5508,7 +5508,7 @@ def Onlineordersystem(request, admin_id):
         messenger=request.GET.get('messenger', '')
         messengerredirect=request.GET.get('messengerredirect', '')
         if messengerredirect:
-            user_details_url = "https://graph.facebook.com/v15.0/me/?fields=first_name,last_name,id&access_token=%"%os.environ.get('USER_ACCESS_TOKEN')
+            user_details_url = "https://graph.facebook.com/v15.0/me/?fields=first_name,last_name,id&access_token=%s"%os.environ.get('USER_ACCESS_TOKEN')
             user_details_params = {'fields':'first_name, last_name,id', 'access_token':os.environ.get('USER_ACCESS_TOKEN')} 
             user_details = requests.get(user_details_url, user_details_params).json() 
             fbidi=user_details['id']
