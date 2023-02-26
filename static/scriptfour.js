6dab0e103c4b2ae77ee963ea068ab26c371c59a1
Appey30 <appey_30@yahoo.com>
2023-02-26 16:15:53 +0800

    refine

--- static/scriptfour.js
+++ static/scriptfour.js
@@ -35,8 +35,8 @@ video.addEventListener('loadedmetadata', () => {
 
 function performRecognition(detections) {
   const canvas = document.createElement('canvas')
-  canvas.width = detections.inputSize.width
-  canvas.height = detections.inputSize.height
+  canvas.width = video.width
+  canvas.height = video.height
   const ctx = canvas.getContext('2d')
   detections.detections.forEach(detection => {
     const box = detection.detection.box
