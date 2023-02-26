3f34180ad2a94f76501d8cd3f150518e1b176e1e
Appey30 <appey_30@yahoo.com>
2023-02-26 21:00:24 +0800

    refine

--- static/scriptfour.js
+++ static/scriptfour.js
@@ -39,12 +39,12 @@ function startVideo() {
 function performRecognition(result) {
 alert('result:  '+JSON.stringify(result))
 alert('result.detection:  '+JSON.stringify(result[0].detection))
-alert('result.detection.length:  '+result.detection.length)
+
   const canvas = document.createElement('canvas')
   canvas.width = video.width
   canvas.height = video.height
   const ctx = canvas.getContext('2d')
-  if (result && result[0].detection && result[0].detection.length > 0) {
+  if (result && result[0].detection) {
   alert('detecteddetecteddetecteddetecteddetected')
     const faces = result.detection[0].detections
 alert('facesfacesfacesfaces:  '+JSON.stringify(faces))
