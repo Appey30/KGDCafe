aec5cfc86b598cceb5e97a1bbfe6dceddacc2b5f
Appey30 <appey_30@yahoo.com>
2023-02-26 12:24:49 +0800

    refine

--- static/scripttwo.js
+++ static/scripttwo.js
@@ -14,7 +14,7 @@ function startVideo() {
     err => console.error(err)
   )
 }
-
+var resizedDetections='';
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
@@ -22,7 +22,7 @@ video.addEventListener('play', () => {
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
