886625ad2895939721b0be04c81d994344f4b23c
Appey30 <appey_30@yahoo.com>
2023-02-26 14:03:35 +0800

    refine

--- static/scripttwo.js
+++ static/scripttwo.js
@@ -28,9 +28,9 @@ faceapi.detectAllFaces(canvas, options).withFaceLandmarks().withFaceExpressions(
     const resizedResults = faceapi.resizeResults(results, { width: video.videoWidth, height: video.videoHeight });
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
     faceapi.draw.drawDetections(canvas, resizedResults);
-
+    var itoDataURL = canvas.toDataURL()
     // Perform face recognition using unique identifier
-    performRecognition(resizedDetections)
+    performRecognition(itoDataURL);
   })
   .catch(function(error) {
     console.error(error);
@@ -43,21 +43,9 @@ faceapi.detectAllFaces(canvas, options).withFaceLandmarks().withFaceExpressions(
    
 
 // Perform face recognition using unique identifier
-function performRecognition(detections) {
-  // Convert the face image to a base64-encoded string
-  const canvas = document.createElement('canvas');
-  canvas.width = video.width;
-  canvas.height = video.height;
-  const ctx = canvas.getContext('2d');
-  detections.detections.forEach(function(detection) {
-    const box = detection.detection.box;
-    const x = box.x < 0 ? 0 : box.x;
-    const y = box.y < 0 ? 0 : box.y;
-    const width = box.x + box.width > canvas.width ? canvas.width - box.x : box.width;
-    const height = box.y + box.height > canvas.height ? canvas.height - box.y : box.height;
-    ctx.drawImage(video, x, y, width, height, box.x, box.y, width, height);
-  });
-  const base64Image = canvas.toDataURL()
+function performRecognition(toDataURL) {
+
+  const base64Image = toDataURL;
 
   // Make an API call to retrieve the unique identifier of the employee associated with the detected face
   $.ajax({
