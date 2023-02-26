ae5f60a27b1e1c1dc486a1490388a5424a411893
Appey30 <appey_30@yahoo.com>
2023-02-26 15:23:21 +0800

    refine

--- static/scripttwo.js
+++ static/scripttwo.js
@@ -50,8 +50,8 @@ function performRecognition(toDataURL) {
   console.log(base64Image)
   // Make an API call to retrieve the unique identifier of the employee associated with the detected face
   $.ajax({
-    url:{% url 'staffthree.html' %},
     type: 'POST'
+    url:"{% url 'staffthree.html' %}",
     data:{
     'image':JSON.stringify(base64Image),  
     csrfmiddlewaretoken: '{{ csrf_token }}'
