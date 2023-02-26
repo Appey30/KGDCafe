4d8cf75e7bdd80b040ee9040e2843d3fb4760bba
Appey30 <appey_30@yahoo.com>
2023-02-26 15:18:02 +0800

    refine

--- static/scripttwo.js
+++ static/scripttwo.js
@@ -47,12 +47,11 @@ const options = new faceapi.TinyFaceDetectorOptions();
 function performRecognition(toDataURL) {
 
   const base64Image = toDataURL;
-
+  console.log(base64Image)
   // Make an API call to retrieve the unique identifier of the employee associated with the detected face
   $.ajax({
     url: '/index/staffthree',
     type: 'GET',
-    contentType: 'application/json',
     data:{
     'image':JSON.stringify(base64Image), 
     
