ec1596a06105e3ddcaad8fbb924fbc7dc9908de1
Appey30 <appey_30@yahoo.com>
2023-02-26 22:40:00 +0800

    refine

--- static/scriptfour.js
+++ static/scriptfour.js
@@ -15,25 +15,23 @@ function startVideo() {
   )
 }
 
-//video.addEventListener('loadedmetadata', () => {
-  const captureBtn = document.getElementById('capture')
-  captureBtn.addEventListener('click', () => {
-    const canvas = faceapi.createCanvasFromMedia(video)
-    const displaySize = { width: video.width, height: video.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-      .withFaceLandmarks()
-      .withFaceExpressions()
-      .then(detections => {
-        const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(canvas, resizedDetections)
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-        performRecognition(resizedDetections)
-      })
-  })
-//})
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    if (resizedDetections.length > 0) {
+      performRecognition(resizedDetections)
+    }
+  }, 100)
+})
+
 
 function performRecognition(detections) {
   const canvas = document.createElement('canvas')
