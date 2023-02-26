72e5d26b25c0cde89627adcb5406df3804ccfd4e
Appey30 <appey_30@yahoo.com>
2023-02-26 23:18:10 +0800

    refine

--- static/scriptfour.js
+++ static/scriptfour.js
@@ -27,6 +27,7 @@ video.addEventListener('play', () => {
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     if (resizedDetections.length > 0) {
       performRecognition(resizedDetections)
     }
