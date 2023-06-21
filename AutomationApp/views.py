0964f3fd1daa1822a29f274101fbb17da01fe1b2
Appey30 <appey_30@yahoo.com>
2023-06-22 02:49:09 +0800

    refine

--- AutomationApp/views.py
+++ AutomationApp/views.py
@@ -5520,6 +5520,8 @@ def customize(request):
         print('body: ',body)
         title_bold = 'bold' if request.POST.get('title_bold') == 'on' else ''
         print('title_bold: ',title_bold)
+        fontStyles = 'bold' if request.POST.get('fontStylesname') == 'on' else ''
+        print('fontStyles: ',fontStyles)
         title_italic = 'italic' if request.POST.get('title_italic') == 'on' else ''
         print('title_italic: ',title_italic)
         title_underline = 'underline' if request.POST.get('title_underline') == 'on' else ''
